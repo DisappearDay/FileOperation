17ad58037503d284c15d836c001cd0f2a71843b4
DisappearDay <117563652+DisappearDay@users.noreply.github.com>
2023-05-04 23:32:47 +0800

    Update test.c

--- test.c
+++ test.c
@@ -66,3 +66,115 @@ int main()
 	fputc(ch,stdout);
 	return 0;
 }
+//int main()
+//{
+//	char arr[1024] = { 0 };
+//	FILE* pf = fopen("text.txt", "r");
+//	if (pf==NULL)
+//	{
+//		return 0;
+//	}
+//
+//	fgets(arr, 1024, pf);
+//  puts(pf);
+//	printf("%s", arr);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[1024] = { 0 };
+//	FILE* pf = fopen("text.txt", "w");
+//	if (pf==NULL)
+//	{
+//		return 0;
+//	}
+//
+//	fputs("hello\n", pf);
+//	fputs("world",pf);
+//
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr[1024] = { 0 };
+//	//fgets(arr,1024,stdin);
+//	//fputs(arr, stdout);
+//
+//	gets(arr);  //等价于上面的
+//	puts(arr);
+//
+//	return 0;
+//
+//}
+
+struct S
+{
+	int a;
+	float b;
+	char arr[1024];
+};
+
+
+//int main()
+//{
+//	struct S s = { 10,3.14,"abcdef" };
+//	FILE* pf = fopen("text.txt", "w");
+//	if (pf==NULL) 
+//	{
+//		return 0;
+//	}
+//	
+//	fprintf(pf,"%d\n""%f\n""%s",s.a,s.b,s.arr);
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	struct S s = {0};
+//	FILE* pf = fopen("text.txt", "r");
+//	if (pf == NULL)
+//	{
+//		return 0;
+//	}
+//  //是针对所有输入/输出流 格式化输入/输出语句
+//	fscanf(pf, "%d\n""%f\n""%s", &(s.a), &(s.b), s.arr);
+//	printf("%d " "%.2f " "%s ",s.a,s.b,s.arr);
+//
+//	fclose(pf);
+//	pf = NULL;
+//
+//	return 0;
+//}
+
+int main()
+{
+	struct S s = { 10,3.14,"abcdef" };
+	char arr[1024] = { 0 };
+	FILE* pf = fopen("text.txt", "w");
+	if (pf==NULL) 
+	{
+		return 0;
+	}
+	//把格式化的数据输出成（存储到）字符串中
+	sprintf(arr,"%d\n""%f\n""%s",s.a,s.b,s.arr);
+	printf("%s\n", arr);
+
+	fclose(pf);
+	pf = NULL;
+
+	return 0;
+}
